09b14209278959c84db4d998e1dcd108dbd9597b
L-Ghost <luis.arantes.sp@gmail.com>
2019-12-10 01:13:43 -0300

    Day 01 - Part II

--- day01.rb
+++ day01.rb
@@ -1,4 +1,14 @@
+fuel = File.open('./day01.txt').map { |num| (num.to_i / 3) - 2 }.inject(:+)
+puts 'SIMPLE FUEL --> ' + fuel.to_s
 
-f = File.open('./day01.txt')
-fuel =  f.map { |num| (num.to_i / 3) - 2 }.inject(:+)
-puts fuel
+def calculate_fuel(input)
+  required_fuel = (input / 3) - 2
+  return required_fuel + calculate_fuel(required_fuel) if required_fuel > 0
+
+  0
+end
+
+total_fuel = File.open('./day01.txt').map do |num|
+  calculate_fuel(num.to_i)
+end.inject(:+)
+puts 'TOTAL FUEL --> ' + total_fuel.to_s
