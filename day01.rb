74c3f471c1dc076aed0e92896d90b91f7215df65
L-Ghost <luis.arantes.sp@gmail.com>
2019-12-10 01:26:24 -0300

    Refactor Day 01

--- day01.rb
+++ day01.rb
@@ -1,14 +1,18 @@
-fuel = File.open('./day01.txt').map { |num| (num.to_i / 3) - 2 }.inject(:+)
-puts 'SIMPLE FUEL --> ' + fuel.to_s
+modules_masses = File.open('./day01.txt').map(&:to_i)
 
-def calculate_fuel(input)
-  required_fuel = (input / 3) - 2
+def fuel(mass)
+  mass / 3 - 2
+end
+
+def calculate_fuel(mass)
+  required_fuel = fuel(mass)
   return required_fuel + calculate_fuel(required_fuel) if required_fuel > 0
 
   0
 end
 
-total_fuel = File.open('./day01.txt').map do |num|
-  calculate_fuel(num.to_i)
-end.inject(:+)
+fuel_sum = modules_masses.map { |mass| fuel(mass) }.inject(:+)
+total_fuel = modules_masses.map { |mass| calculate_fuel(mass) }.inject(:+)
+
+puts 'SIMPLE FUEL --> ' + fuel_sum.to_s
 puts 'TOTAL FUEL --> ' + total_fuel.to_s
